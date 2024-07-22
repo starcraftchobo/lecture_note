5bdc074431bf629c25a06bf303822fd9c1cc0798
starcraftchobo <madeinburundi@gmail.com>
2024-07-22 11:23:23 +0900

    upload

--- codes/08-data-structure/02-list.py
+++ codes/08-data-structure/02-list.py
@@ -1,32 +1,61 @@
 # append
 my_list = [1, 2, 3]
+my_list.append(4)
+print(my_list)
 
 # extend
 my_list = [1, 2, 3]
-
+my_list.extend([4, 5, 6])
+print(my_list)
+# my_list.extend(5)
+# print(my_list)
+my_list.append([9, 9, 9])
+print(my_list)
+# my_list.append(9, 9)
 # insert
 my_list = [1, 2, 3]
-
+my_list.insert(1,5)
+print(my_list)
 # remove
-my_list = [1, 2, 3]
+my_list = [1, 2, 3, 2, 2, 2]
+my_list.remove(2)
+print(my_list)
 
 # pop
 my_list = [1, 2, 3, 4, 5]
+item1 = my_list.pop()
+item2 = my_list.pop(0)
+
+print(item1)
+print(item2)
+print(my_list)
 
 # clear
 my_list = [1, 2, 3]
+my_list.clear()
+print(my_list)
 
 # index
 my_list = [1, 2, 3]
+index = my_list.index(2)
+print(index)
 
 # count
 my_list = [1, 2, 2, 3, 3, 3]
+count = my_list.count(3)
+print(count)
 
 # reverse
 my_list = [1, 3, 2, 8, 1, 9]
+my_list.reverse()
+print(my_list)
+print(my_list.reverse())
 
 # sort
 my_list = [3, 2, 100, 1]
-
+my_list.sort()
+print(my_list)
 
 # sort(내림차순 정렬)
+my_list.sort(reverse=True)
+print(my_list)
--- codes/08-data-structure/99-copy.py
+++ codes/08-data-structure/99-copy.py
@@ -1,7 +1,42 @@
 # 할당
+a = [1, 2, 3, 4]
+b = a
+b[0] = 100
+print(b)
+print(a)# a는 안바뀐다!!!!!!!!!!가아니넹...
+
+#int is immutable
+a = 20
+b = a
+b = 10
+print(a)
+print(b)
 
 # 얕은 복사
+a = [1, 2, 3]
+b = a[:]
+c = a.copy()
+
+b[0] = 100
+c[0] = 999
+print(a)
+print(b)
+print(c)
 
 # 얕은 복사의 한계
+a = [1, 2, [3, 4, 5]]
+b= a[:]
+
+b[0] = 999
+b[2][1] = 1234
+print(a)
+print(b)
 
 # 깊은 복사
+import copy
+a = [1, 2, [3, 4, 5]]
+b= copy.deepcopy(a)
+
+b[2][1] = 1234
+print(a)
+print(b)
