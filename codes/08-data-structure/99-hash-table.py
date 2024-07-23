0de9f674d99a3d25d579500bfbedfd00d2ef34e0
eduharry <harry@forestsoft.co.kr>
2024-07-23 11:01:10 +0900

    done 0723

--- codes/08-data-structure/99-hash-table.py
+++ codes/08-data-structure/99-hash-table.py
@@ -1,17 +1,16 @@
 # 정수
 my_set = {3, 2, 1, 9, 100, 4, 87, 39, 10, 52}
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set.pop())
-# print(my_set)
-
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set.pop())
+print(my_set)
 
 # 문자열
 my_str_set = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
@@ -21,10 +20,10 @@ my_str_set = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'}
 # print(my_str_set.pop())
 # print(my_str_set.pop())
 
-# print(hash(1))
-# print(hash(1))
-# print(hash('a'))
-# print(hash('a'))
+print(hash(1))
+print(hash(1))
+print(hash('a'))
+print(hash('a'))
 
 # print(hash(1))
 # print(hash(1.0))
